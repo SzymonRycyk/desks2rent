d3af03b3eb669fb3abb152cc60dad7729520b843
Szymon Rycyk <szymon.rycyk@outlook.com>
2020-09-26 12:47:14 +0200

    czysty projekt

---
+++ src/main/java/pl/sda/projekt_koncowy/desks2rent/Desks2rentApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.projekt_koncowy.desks2rent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desks2rentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Desks2rentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/projekt_koncowy/desks2rent/Desks2rentApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.projekt_koncowy.desks2rent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desks2rentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
